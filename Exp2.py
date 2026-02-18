91ddf97a0852137055981814e5d804e33c5205c0
hussain72007 <zakirhussainagharia.2007@gmail.com>
2026-02-18 08:54:29 +0530

    Create Exp2.py

---
+++ Exp2.py
@@ -0,0 +1,20 @@
+print("1 Circle")
+print("2 Rectangle")
+print("3 Triangle")
+
+c = int(input("choose"))
+if c == 1:
+r = float(input("Radius: "))
+print("Area = ",3.14 *r *r)
+
+elif c == 2:
+1 = float(input("Length: "))
+b = float(input("Breadth: "))
+print("Area =",1*b)
+elif c == 3:
+
+
+b = float(input("Base: "))
+h = float(input("Height: "))
+print("area = ",1/2*b*h)
+
